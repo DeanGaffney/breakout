e6f1232e531dd68e917a62a977414ed173c33350
Dean _Gaffney <gaffmasterflex123@gmail.com>
2017-10-19 17:05:08 +0100

    main folder

---
+++ js/main/main.js
@@ -0,0 +1,21 @@
+var canvas = document.getElementById("renderCanvas");
+// Load the BABYLON 3D engine
+var engine = new BABYLON.Engine(canvas, true);
+
+
+var gameScene = createGameScene();
+var splashScreen = createSplashScene();
+var gameOverScreen = createGameOverScene();
+
+//add game scens to game scene array
+Game.gameScenes.push(splashScreen);
+Game.gameScenes.push(gameScene);
+Game.gameScenes.push(gameOverScreen);
+
+engine.runRenderLoop(function(){
+      Game.gameScenes[Game.activeScene].renderLoop();
+});
+
+window.addEventListener("resize", function () {
+  engine.resize();
+});
